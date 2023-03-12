3b25d9e732e3401f01b1dc59236408570bb0b1a4
trumanpurnell <truman.purnell@gmail.com>
2023-03-12 01:15:29 -0700

    finalize logo

--- slide-app/Slide/Views/Intro/SplashView.swift
+++ slide-app/Slide/Views/Intro/SplashView.swift
@@ -18,26 +18,21 @@ struct SplashView<Content: View>: View {
                 Color("Background").ignoresSafeArea()
                 
                 VStack {
-                    Image("GhostLogo")
+                    Image("AppLogo")
                         .resizable()
                         .frame(width: 128, height: 128)
                         .scaleEffect(self.size)
                         .opacity(self.opacity)
                         .onAppear {
-                            withAnimation(.easeIn(duration: 1.2)) {
-                                self.size = 0.9
+                            withAnimation(.easeInOut(duration: 1.2)) {
+                                self.size = 1.5
                                 self.opacity = 1.0
                             }
                         }
-                        .padding(.top, UIScreen.main.bounds.height / 9)
-                    
-                    Spacer()
                 }
             }.onAppear {
                 DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                    withAnimation(.easeIn(duration: 0.5)) {
-                        self.isComplete.toggle()
-                    }
+                    self.isComplete.toggle()
                 }
             }
         }
