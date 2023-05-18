372287c4553b2577b62f337056a5bd4a6d44608c
trumanpurnell <truman.purnell@gmail.com>
2023-05-18 01:25:41 -0600

    grabbing APNS token

--- front/ios/Slide/App/SlideApp.swift
+++ front/ios/Slide/App/SlideApp.swift
@@ -3,6 +3,8 @@ import SwiftUI
 
 @main
 struct SlideApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+
     let userModel = UserModel()
     let introModel = IntroModel()
     let cameraModel = CameraModel()
@@ -16,5 +18,23 @@ struct SlideApp: App {
                 .environmentObject(introModel)
                 .environmentObject(webRTCModel)
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success,_ in
+            print("Success: ", success)
+        }
+        
+        UIApplication.shared.registerForRemoteNotifications()
+        return true
+    }
+    
+    func application(_ application: UIApplication,
+                didRegisterForRemoteNotificationsWithDeviceToken
+                    deviceToken: Data) {
+        print("Token: ", deviceToken)
     }
 }
