d6ec77802945501846c66e896406312a127959a4
trumanpurnell <truman.purnell@gmail.com>
2023-11-26 01:56:52 -0700

    Put back main.go, bump release

--- go/main.go
+++ go/main.go
@@ -1,25 +1,87 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bb-labs/sage/presign"
+	"github.com/bb-labs/sage/signaling"
+	"github.com/bb-labs/sage/user"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	// Create the server
+	ctx := context.Background()
 	router := gin.Default()
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// Connect to db
+	uri := fmt.Sprintf("mongodb://%s:%s@%s:%s",
+		os.Getenv("MONGO_INITDB_ROOT_USERNAME"),
+		os.Getenv("MONGO_INITDB_ROOT_PASSWORD"),
+		os.Getenv("DB_CONTAINER_NAME"),
+		os.Getenv("DB_PORT"))
+
+	db, err := mongo.NewClient(options.Client().ApplyURI(uri))
+
+	if err != nil {
+		log.Fatalf("err: %v", err)
+	}
+
+	err = db.Connect(ctx)
+	if err != nil {
+		log.Fatalf("err: %v", err)
+	}
+
+	// Initialize the signaling hub
+	upgrader := websocket.Upgrader{}
+	hub, err := signaling.NewHub(db, ctx)
+	if err != nil {
+		log.Fatalf("err initializing hub: %v", err)
+	}
+	go hub.Run()
+
+	// Connect to S3 for presigning
+	sdkConfig, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		log.Fatalf("err: %v", err)
+	}
+
+	s3Client := s3.NewFromConfig(sdkConfig)
+	presignClient := s3.NewPresignClient(s3Client)
+
+	// Cleanup any errors
+	defer func() {
+		db.Disconnect(ctx)
+	}()
 
 	// Test route for health check
 	router.GET("/ping", func(c *gin.Context) {
 		log.Println("ping")
 		c.JSON(200, gin.H{
-			"env": os.Environ(),
+			"message": "pong",
 		})
 	})
 
+	// Setup routes for user actions
+	router.POST("/user", user.HandleCreateUser(db))
+	router.POST("/locate", user.HandleLocation(db))
+
+	// Setup routes for presigning
+	router.GET("/presign", presign.HandlePresign(presignClient))
+
+	// Setup routes for sessions and wss routing
+	router.POST("/route", signaling.HandleRoute(hub))
+	router.GET("/session", signaling.HandleSession(upgrader, hub))
+
 	// Run the server
-	router.Run(":3000")
+	router.Run(fmt.Sprintf(":%s", os.Getenv("APP_PORT")))
 }
