5d87c07cbffb92138371afde52ef52cfa161e67f
trumanpurnell <truman.purnell@gmail.com>
2024-03-23 11:24:03 -0600

    Add S3 <> ENV link

--- awyes.py
+++ awyes.py
@@ -1,17 +1,20 @@
 import boto3
 import docker
-import subprocess
+import pathlib
 
-ecs = boto3.client("ecs")
 ssm = boto3.client("ssm")
 iam = boto3.client("iam")
-ec2 = boto3.client("ec2")
 secrets = boto3.client("secretsmanager")
 
-ecs_tasks_stopped = ecs.get_waiter("tasks_stopped")
-ssm_waiter = ssm.get_waiter("command_executed")
-ec2_waiter = ec2.get_waiter("instance_status_ok")
+s3 = boto3.client("s3")
+bucket_exists = s3.get_waiter("bucket_exists")
+object_exists = s3.get_waiter("object_exists")
 
+ecs = boto3.client("ecs")
+ecs_tasks_stopped = ecs.get_waiter("tasks_stopped")
+
+ec2 = boto3.client("ec2")
+ec2_waiter = ec2.get_waiter("instance_status_ok")
 
 route53 = boto3.client("route53")
 record_sets_changed = route53.get_waiter("resource_record_sets_changed")
@@ -32,3 +35,6 @@ try:
     image = docker.from_env().images
 except:
     pass
+
+read_text = lambda path: pathlib.Path(path).read_text()
+read_bytes = lambda path: pathlib.Path(path).read_bytes()
