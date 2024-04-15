00ed94af0ff9bfe627eecb64c48422dcd58d5342
trumanpurnell <truman.purnell@gmail.com>
2024-04-14 22:19:21 -0600

    Remove old slide app

---
+++ ios/SageApp/SageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SageAppApp.swift
+//  SageApp
+//
+//  Created by Truman Purnell on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
